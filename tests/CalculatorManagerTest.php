830261414408b871fcab7622d6e3c7ee86d01835
Syed Abidur Rahman <abidur.rahman@shohoz.com>
2021-10-23 00:15:59 +0600

    Add the test cases for the given transactional inputs and outputs.

--- tests/CalculatorManagerTest.php
+++ tests/CalculatorManagerTest.php
@@ -9,6 +9,7 @@ use Paysera\CommissionTask\CalculatorManager;
 use Paysera\CommissionTask\CommissionRules\DepositRule;
 use Paysera\CommissionTask\CommissionRules\WithdrawBusinessRule;
 use Paysera\CommissionTask\CommissionRules\WithdrawPrivateRule;
+use Paysera\CommissionTask\Service\DataReader\InputData;
 use Paysera\CommissionTask\Service\ExchangeRate\RateService;
 use Paysera\CommissionTask\Transactions\Collection as TransactionCollection;
 
@@ -28,6 +29,64 @@ class CalculatorManagerTest extends TestCase
             ->applyAllRules();
 
         $this->assertEquals(0, count($manager->getAllTransactions()));
+    }
+
+    /**
+     * @param $transactionDate
+     * @param $userId
+     * @param $userType
+     * @param $operationType
+     * @param $amount
+     * @param $currency
+     * @param $commission
+     * @param $rate
+     * @dataProvider dataProviderForAddTesting
+     */
+    public function testEveryTransactionWithMatchingInputAndOutput($transactionDate, $userId, $userType, $operationType, $amount, $currency, $commission, $rate)
+    {
+        $rateService = $this->mockRateService();
+        $rateService->method('getRate')->willReturn($rate);
+
+        $collection = new TransactionCollection([
+            $this->getSampleTransaction($transactionDate, $userId, $userType, $operationType, $amount, $currency)
+        ]);
+
+        $manager = new CalculatorManager();
+        $calculateTransaction = $manager->addTransactions($collection)
+            ->addRule(new DepositRule())
+            ->addRule(new WithdrawBusinessRule())
+            ->addRule(new WithdrawPrivateRule($rateService))
+            ->applyAllRules()
+            ->getAllTransactions();
+
+        $this->assertEquals(1, count($calculateTransaction));
+        $this->assertEquals($commission, round($calculateTransaction[0]->getCommission(), 2));
+    }
+
+    public function dataProviderForAddTesting(): InputOutputFileIterator
+    {
+        return new InputOutputFileIterator('./input.csv', './output.csv');
+    }
+
+    /**
+     * @param $transactionDate
+     * @param $userId
+     * @param $userType
+     * @param $operationType
+     * @param $amount
+     * @param $currency
+     * @return InputData
+     */
+    public function getSampleTransaction($transactionDate, $userId, $userType, $operationType, $amount, $currency): InputData
+    {
+        $inputObject = new InputData();
+        $inputObject->setTransactionDate($transactionDate);
+        $inputObject->setUserIdentification($userId);
+        $inputObject->setUserType($userType);
+        $inputObject->setOperationType($operationType);
+        $inputObject->setOperationAmount((float) $amount);
+        $inputObject->setOperationCurrency($currency);
+        return $inputObject;
     }
 
     /**
